747e23393747e28285f188eadea202516a485616
YngvN <y.nykaas@gmail.com>
2024-11-25 17:08:00 +0100

    Many fixes and more styling

--- src/pages/home/home.mjs
+++ src/pages/home/home.mjs
@@ -5,15 +5,4 @@ document.addEventListener("DOMContentLoaded", () => {
     buildListingTiles().then(() => {
 
     });
-    // Event listener to remove all modal backdrops when a modal is hidden
-    document.addEventListener("hidden.bs.modal", () => {
-        document.querySelectorAll(".modal-backdrop").forEach((backdrop) => backdrop.remove());
-    });
-
-    // Also, remove backdrops when clicking outside the modal
-    document.addEventListener("click", (event) => {
-        if (event.target.classList.contains("modal")) {
-            document.querySelectorAll(".modal-backdrop").forEach((backdrop) => backdrop.remove());
-        }
-    });
 });
